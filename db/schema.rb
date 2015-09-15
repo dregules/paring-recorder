72f3072859531ceda2210ad858b534660d6bc96a
Christopher Alcock <christopher.alcock@gmail.com>
2015-09-15 17:20:28 +0100

    dates selected showing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915125138) do
+ActiveRecord::Schema.define(version: 20150915161606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20150915125138) do
     t.datetime "updated_at",  null: false
     t.text     "studentOne"
     t.text     "studentTwo"
-    t.date     "pairingDate"
+    t.text     "pairingDate"
   end
 
   create_table "students", force: :cascade do |t|
