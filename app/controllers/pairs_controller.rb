737662c889aba1b39660a0c749b6cbe98eed7bbc
Diego Regules <diegoregules@gmail.com>
2015-09-15 15:06:34 +0100

    Pair Persisting in DB

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -3,7 +3,16 @@ class PairsController < ApplicationController
     @time = Time.new
     start_date = Time.new(2015,8,31,1,00,00)
     @week_number = ((@time - start_date)/(60*60*24*7)+1).to_i
-    @students = ['Chris','Diego','Tommy','Pablo']
+    @students = ['Chris','Diego','Tommy', 'Pablo']
     @remaining_students
   end
+  def create
+    @pair = Pair.create(pair_params)
+    redirect_to pairs_path
+  end
+
+  def pair_params
+    params.require(:pair).permit(:studentOne, :studentTwo)
+  end
+
 end
