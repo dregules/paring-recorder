4378a67a3ecb1ef6aca044fe7e6dacc7841b7b7f
Diego Regules <diegoregules@gmail.com>
2015-09-15 12:01:25 +0100

    drop downs take names from controller

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -1,7 +1,8 @@
 class PairsController < ApplicationController
   def index
     @time = Time.new
-    @start_date = Time.new(2015,8,31,1,00,00)
-    @week_number = ((@time - @start_date)/(60*60*24*7)+1).to_i
+    start_date = Time.new(2015,8,31,1,00,00)
+    @week_number = ((@time - start_date)/(60*60*24*7)+1).to_i
+    @students = ['Chris','Diego','Tommy','Pablo']
   end
 end
--- spec/features/pairing_input_feature_spec.rb
+++ spec/features/pairing_input_feature_spec.rb
@@ -6,12 +6,18 @@ feature 'pairing input for the day' do
     expect(page).to have_select('studentOne')
     expect(page).to have_select('studentTwo')
   end
+
   scenario 'the LEFT dropdown menu  contains all the students' do
-
+    visit pairs_path
+    expect(page).to have_select('studentOne', options: ['Chris', 'Diego', 'Tommy', 'Pablo'])
   end
+
   scenario 'the RIGHT dropdown menu contains remaining students' do
-
+    visit pairs_path
+    select "Chris", from: 'studentOne'
+    expect(page).to have_select('studentTwo', options: ['Diego', 'Tommy', 'Pablo'])
   end
+
   context 'recording the pair input' do
     scenario 'the pairing result is displayed' do
 
